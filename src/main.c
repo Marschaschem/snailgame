9d888f7c2422ad7c0b0d32468f292eb1e1fec47e
Taisija <taisija@Taisijas-MacBook-Air.local>
2024-04-30 19:57:10 +0200

    Changed text. Again.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "stdio.h"
 
 int main(void) {
-    printf("This game isn't actually about a snail. But it will be!\n");
+    printf("This game isn't actually about a snail. But it will be! (NO. IT'S A SLUG!)\n");
     return 0;
 }
